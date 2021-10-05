91bcffd2c2ec29346ead17a7c2199cd3b61e44e3
angel <angel.batlles@kumux.io>
2021-10-05 12:12:34 +0200

    make it better

---
+++ config/database.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "ec2-52-7-228-45.compute-1.amazonaws.com"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "dcgkvf1qfmm0ju"),
+        username: env("DATABASE_USERNAME", "gahbjdbbstwtfr"),
+        password: env(
+          "DATABASE_PASSWORD",
+          "1a8393cad953128940ce1ee335fd1a8b69d0745046c5cb4a53f3257a915bc5d9"
+        ),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
+      options: {},
+    },
+  },
+});
---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    upload: {
+        provider: "cloudinary",
+        providerOptions: {
+            cloud_name: "med-ia",
+            api_key: "878828732769964",
+            api_secret: "zpPVoCnthZgj4jOYuIGoNrtJm9o",
+        },
+    },
+});
